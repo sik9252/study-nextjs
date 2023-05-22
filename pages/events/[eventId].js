2389e51cae7af1ca115afaa74d6ecbdec2549d4b
sik9252 <sik9252@naver.com>
2023-05-23 02:11:47 +0900

    feat: 이벤트 상세페이지에 생성한 컴포넌트들과 이벤트 정보 삽입

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,7 +1,35 @@
+import { Fragment } from "react";
+import { useRouter } from "next/router";
+import { getEventById } from "../../dummy-data";
+
+/** components */
+import EventSummary from "../../components/event-detail/EventSummary";
+import EventLogistics from "../../components/event-detail/EventLogistics";
+import EventContent from "../../components/event-detail/EventContent";
+
 function EventDetailPage() {
+  const router = useRouter();
+  // url에서 eventId 파싱
+  const eventId = router.query.eventId;
+  // 파싱한 eventId를 통해 해당 이벤트의 정보를 가져옴
+  const eventDetails = getEventById(eventId);
+
+  if (!eventDetails) {
+    return <h1>이벤트가 존재하지 않습니다!</h1>;
+  }
+
   return (
     <div>
-      <h1>Event Detail</h1>
+      <EventSummary title={eventDetails.title} />
+      <EventLogistics
+        date={eventDetails.date}
+        address={eventDetails.location}
+        image={eventDetails.image}
+        imageAlt={eventDetails.title}
+      />
+      <EventContent>
+        <p>{eventDetails.description}</p>
+      </EventContent>
     </div>
   );
 }
