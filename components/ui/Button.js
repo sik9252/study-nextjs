4858ce30212025ba0d00eafdd409d394f0489b99
sik9252 <sik9252@naver.com>
2023-05-24 01:13:49 +0900

    feat: 이벤트 검색 UI 컴포넌트 추가

--- components/ui/Button.js
+++ components/ui/Button.js
@@ -4,10 +4,20 @@ import classes from "./Button.module.css";
 function Button(props) {
   const { link, children } = props;
 
+  // 페이지 이동 버튼
+  if (props.link) {
+    return (
+      <Link href={link} className={classes.btn}>
+        {children}
+      </Link>
+    );
+  }
+
+  // 기본 버튼
   return (
-    <Link href={link} className={classes.btn}>
-      {children}
-    </Link>
+    <button className={classes.btn} onClick={props.onClick}>
+      {props.onClick}
+    </button>
   );
 }
 
--- pages/events/index.js
+++ pages/events/index.js
@@ -2,12 +2,14 @@ import { getAllEvents } from "../../dummy-data";
 
 /** components */
 import EventList from "../../components/events/EventList";
+import EventSearch from "../../components/events/EventSearch";
 
 function AllEventsPage() {
   const eventLists = getAllEvents();
 
   return (
     <div>
+      <EventSearch />
       <EventList items={eventLists} />
     </div>
   );
